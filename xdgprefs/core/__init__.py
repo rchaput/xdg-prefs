9e54052540445c54b4a2a50c778de461bca1d6a2
rchaput <rchaput.pro@gmail.com>
2019-07-30 11:26:17 +0200

    [Quickfix] Added imports in core/__init__.py

--- xdgprefs/core/__init__.py
+++ xdgprefs/core/__init__.py
@@ -1,1 +1,13 @@
 from .app_database import AppDatabase
+from .desktop_entry import DesktopEntry
+from .mime_database import MimeDatabase
+from .mime_type import MimeType
+from . import os_env
+from . import xdg_mime_wrapper
+
+__all__ = ['AppDatabase',
+           'DesktopEntry',
+           'MimeDatabase',
+           'MimeType',
+           'os_env',
+           'xdg_mime_wrapper']
