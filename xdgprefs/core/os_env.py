82464f1c3d42f988411bc691dc8026e2823adfa7
rchaput <rchaput.pro@gmail.com>
2019-10-19 16:45:35 +0200

    [XP-006] Added configparser to read and write the mimeapps.list files. Can change the default app without relying on xdg-mime

--- xdgprefs/core/os_env.py
+++ xdgprefs/core/os_env.py
@@ -13,48 +13,38 @@ import os
 
 def xdg_data_home():
     """Base directory where user specific data files should be stored."""
-    value = os.getenv('XDG_DATA_HOME')
-    if value is None or value == '':
-        value = '$HOME/.local/share/'
+    value = os.getenv('XDG_DATA_HOME') or '$HOME/.local/share/'
     return os.path.expandvars(value)
 
 
 def xdg_config_home():
     """Base directory where user specific config files should be stored."""
-    value = os.getenv('XDG_CONFIG_HOME')
-    if value is None or value == '':
-        value = '$HOME/.config/'
+    value = os.getenv('XDG_CONFIG_HOME') or '$HOME/.config/'
     return os.path.expandvars(value)
 
 
 def xdg_data_dirs():
     """Ordered set of directories for data files."""
-    value = os.getenv('XDG_DATA_DIRS')
-    if value is None or value == '':
-        value = '/usr/local/share/:/usr/share/'
+    value = os.getenv('XDG_DATA_DIRS') or '/usr/local/share/:/usr/share/'
     value = value.split(':')
     return value
 
 
 def xdg_config_dirs():
     """Ordered set of directories for config files."""
-    value = os.getenv('XDG_CONFIG_DIRS')
-    if value is None or value == '':
-        value = '/etc/xdg/'
+    value = os.getenv('XDG_CONFIG_DIRS') or '/etc/xdg/'
     return value.split(':')
 
 
 def xdg_cache_home():
     """Base directory where user specific cached data should be stored."""
-    value = os.getenv('XDG_CACHE_HOME')
-    if value is None or value == '':
-        value = ''
+    value = os.getenv('XDG_CACHE_HOME') or ''
     return os.path.expandvars(value)
 
 
 def xdg_runtime_dir():
     """Base directory for runtime files, such as sockets."""
-    value = os.getenv('XDG_RUNTIME_DIR')
+    value = os.getenv('XDG_RUNTIME_DIR') or ''
     return os.path.expandvars(value)
 
 
@@ -79,6 +69,7 @@ def get_language():
 
     :rtype: str
     """
-    lang = os.getenv('LANGUAGE')
-    lang = lang.split(':')[1]
+    lang = os.getenv('LANGUAGE') or ''
+    if ':' in lang:
+        lang = lang.split(':')[1]
     return lang
